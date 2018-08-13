41e2911d80189a3d3009251c0839c961e24660d9
Mortuza Hossain <mortuzahossain1997@gmail.com>
2018-08-13 15:00:53 +0600

    Complete the algorithm

--- app/src/main/java/com/wordpress/mortuza99/tasbhi/MainActivity.java
+++ app/src/main/java/com/wordpress/mortuza99/tasbhi/MainActivity.java
@@ -2,12 +2,30 @@ package com.wordpress.mortuza99.tasbhi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button counterBtn = findViewById(R.id.counterBtn);
+
+        counterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = counter + 1;
+                counterBtn.setText(String.format("%s%s", getString(R.string.total), String.valueOf(counter)));
+            }
+        });
+
+
     }
 }
